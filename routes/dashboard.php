682471f49e2528729573cc13ae3d1ce2ddc8d6e1
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2026-05-29 17:17:57 -0300

    feat(dashboard): refactor dashboard route to use MessageController for message management

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -2,14 +2,10 @@
 
 use App\Models\ContactMessage;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
 
-Route::get('/dashboard', function () {
-    $messages = ContactMessage::latest()->paginate(10);
+Route::get('/dashboard', [MessageController::class, 'index'])->middleware('auth')->name('dashboard');
 
-    return view('administrator.dashboard', [
-        'title' => 'Dashboard - Fercho Sistemas',
-        'messages' => $messages
-    ]);
 
-    })->name('dashboard')->middleware('auth');
-
+Route::get('/dashboard/message/{message}', [MessageController::class, 'show'])->middleware('auth')->name('dashboard.message');
+Route::delete('/dashboard/message/{message}', [MessageController::class, 'destroy'])->middleware('auth')->name('dashboard.message.destroy');

efabd77040b193fa4c343e1a5185e57632cb4831
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2026-06-24 23:59:44 -0300

    refactor(nav-link): remove underline animation for inactive state

--- resources/views/components/navbar/nav-link.blade.php
+++ resources/views/components/navbar/nav-link.blade.php
@@ -6,6 +6,4 @@
    {{ $attributes->merge(['class' => 'relative px-4 py-2 rounded-xl transition-all duration-300 group ' . ($isActive ? 'text-blue-400 bg-blue-500/10' : 'text-slate-300 hover:text-white hover:bg-slate-900/60')]) }}>
 
     {{ $label }}
-
-    <span class="absolute bottom-1 left-4 right-4 h-0.5 bg-blue-500 origin-left scale-x-0 transition-transform duration-300 group-hover:scale-x-100 {{ $isActive ? 'scale-x-100' : '' }}"></span>
 </a>
